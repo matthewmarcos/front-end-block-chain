78433be673bd2c8559c4962164f4270f2bcf1e29
mamerisawesome <atmalmer23@gmail.com>
2017-11-12 03:31:37 +0800

    Updated login page

--- src/containers/IndexContainer.jsx
+++ src/containers/IndexContainer.jsx
@@ -5,7 +5,22 @@ class IndexContainer extends Component {
     render() {
         return (
             <div className="container">
-                Hi Almer! Im so tired
+                <div className="card">
+                    <div className="card-body">
+                        <h4 className="card-title">Login</h4>
+                        <form action="/api/Agency" method="POST">
+                            <div className="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                            </div>
+                            <div className="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" className="form-control" id="password" placeholder="Enter password" />
+                            </div>
+                            <input className="btn btn-primary" type="submit" value="Login"/>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
