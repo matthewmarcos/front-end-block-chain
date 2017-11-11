fd0839ce1ea4b86e8c51dbf68179e3cb89ce5b52
mamerisawesome <atmalmer23@gmail.com>
2017-11-12 06:59:44 +0800

    Add-citizen modal added

--- src/components/AgencyAddCitizenModal.jsx
+++ src/components/AgencyAddCitizenModal.jsx
@@ -4,30 +4,31 @@ class AgencySettingsModal extends Component {
     render () {
         return (
             <div>
-                <div className="modal" id="modal-target" tabIndex="-1" role="dialog">
+                <div className="modal" id="modal-target-add-citizen" tabIndex="-1" role="dialog">
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h5 className="modal-title">Settings</h5>
+                                <h5 className="modal-title">Add Citizen</h5>
                                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <h2>ADD FIELD</h2>
                                 <div className="form-group">
-                                    <label htmlFor="field-metadata">Field Metadata Key</label>
-                                    <input type="text" className="form-control" id="field-metadata" placeholder="Enter Field Metadata Key" />
+                                    <label htmlFor="citizen-id">ID</label>
+                                    <input type="text" className="form-control" id="citizen-id" placeholder="Enter Citizen ID" />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="field-type">Field Type</label>
-                                    <input type="text" className="form-control" id="field-type" placeholder="Enter Field Type" />
+                                    <label htmlFor="citizen-first-name">First Name</label>
+                                    <input type="text" className="form-control" id="citizen-first-name" placeholder="Enter Citizen First Name" />
                                 </div>
-
-                                <h2>SHARE FIELD</h2>
                                 <div className="form-group">
-                                    <label htmlFor="field-metadata">Agency Name</label>
-                                    <input type="text" className="form-control" id="field-metadata" placeholder="Enter Agency Name" />
+                                    <label htmlFor="citizen-middle-name">Middle Name</label>
+                                    <input type="text" className="form-control" id="citizen-middle-name" placeholder="Enter Citizen First Name" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="citizen-last-name">Last Name</label>
+                                    <input type="text" className="form-control" id="citizen-last-name" placeholder="Enter Citizen First Name" />
                                 </div>
                             </div>
                             <div className="modal-footer">
