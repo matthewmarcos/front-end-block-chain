751d2c2d7a7825eb347cb62e390ef0ff7edd4f10
mamerisawesome <atmalmer23@gmail.com>
2017-11-12 10:50:13 +0800

    Removed buttons for agency container

--- src/containers/AgencyContainer.jsx
+++ src/containers/AgencyContainer.jsx
@@ -20,20 +20,21 @@ class AgencyContainer extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <h1 className="col col-sm-10">{ this.props.user }</h1>
-                    <button type="button" className="btn btn-primary" id="modal-trigger" data-toggle="modal" data-target="#modal-target">
+                    <h1 className="col col-sm-10">{ this.props.user || '' }</h1>
+                    {/*<button type="button" className="btn btn-primary" id="modal-trigger" data-toggle="modal" data-target="#modal-target">
                         TODO OPEN SETTINGS
-                    </button>
+                    </button>*/}
                 </div>
 
                 <div className="row">
-                    <form onSubmit={this.fetchCitizens.bind(this)}>
+                    <h3 className="col col-sm-10">List of Citizens</h3>
+                    {/*<form onSubmit={this.fetchCitizens.bind(this)}>
                         <div className="form-group">
                             <input type="text" className="form-control" placeholder="Search Field" aria-describedby="search-addon" />
                             <button name="filter" className="input-group-addon" id="search-addon">TODO: SEARCH BUTTON ICON</button>
                             <button className="input-group-addon" id="modal-trigger-add-citizen" data-toggle="modal" data-target="#modal-target-add-citizen">Add Something</button>
                         </div>
-                    </form>
+                    </form>*/}
                 </div>
 
                 <div className="row citizen-list">
@@ -61,6 +62,6 @@ class AgencyContainer extends Component {
 export default connect(store => {
     return {
         citizen: store.citizens,
-        user: store.user.user || 'Not Logged In'
+        user: store.user.user || ''
     };
 })(AgencyContainer);
